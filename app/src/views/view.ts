3a31c1f007c00b827678e409a188be6fb7f2617f
mnishimori <mnishimori@yahoo.com.br>
2022-02-04 18:19:05 -0300

    Decorator de propriedade

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -14,8 +14,6 @@ export abstract class View<T> {
         }
     }
 
-    @inspect
-    @logarTempoExecucao(true)
     public update(model: T): void {
         let template = this.template(model);
         this.elemento.innerHTML = template;
